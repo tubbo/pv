72c5851e3d93f28dee729e37004bfdea14828e34
Tom Scott <tubbo@psychedeli.ca>
2012-11-18 02:30:28 -0500

    Add module-level accessors for PivotalTracker and the config

--- lib/pv.rb
+++ lib/pv.rb
@@ -1,6 +1,14 @@
+require 'pv/configuration'
 require 'pv/command'
 require 'pv/version'
+require 'pv/bug_tracker'
 
 module Pv
-  # Your code goes here...
+  def self.config
+    @config ||= Pv::Configuration.new
+  end
+
+  def self.tracker
+    @tracker ||= Pv::BugTracker.new
+  end
 end
