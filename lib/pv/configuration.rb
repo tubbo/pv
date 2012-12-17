d7ae8b2819b925ac74e5a236921e91d9edb671db
Tom Scott <tubbo@psychedeli.ca>
2012-12-17 17:02:54 -0500

    Add project-specific configuration options.
    - Write specs for project-specific configuration, based on the tests
      that already exist for the global config.
    - Create fixture data based on dummy ~/.pv

--- lib/pv/configuration.rb
+++ lib/pv/configuration.rb
@@ -5,8 +5,7 @@ module Pv
     attr_reader :username, :password, :attributes, :project_id, :name
 
     def initialize
-      @path = File.expand_path "~/.pv"
-      @attributes = YAML::load_file @path
+      @attributes = YAML::load_file from_path
       @username = @attributes['username']
       @password = @attributes['password']
       @project_id = @attributes['project_id']
@@ -14,7 +13,20 @@ module Pv
     end
 
     def present?
-      File.exists? @path
+      File.exists? from_path
+    end
+
+  private
+    def from_path
+      File.expand_path yaml_file_location
+    end
+
+    def yaml_file_location
+      if File.exists? "#{Dir.pwd}/.pv"
+        "#{Dir.pwd}/.pv"
+      else
+        "~/.pv"
+      end
     end
   end
 end
--- spec/lib/pv/configuration_spec.rb
+++ spec/lib/pv/configuration_spec.rb
@@ -2,42 +2,79 @@ require 'pv/configuration'
 
 module Pv
   describe Configuration do
-    before do
-      if File.exists? File.expand_path("~/.pv")
-        `cp ~/.pv ~/.pv.safe`
+    context "Using the global configuration file" do
+      before do
+        if File.exists? File.expand_path("~/.pv")
+          `cp ~/.pv ~/.pv.safe`
+        end
+
+        `cp spec/fixtures/pv.yml ~/.pv`
       end
 
-      `cp spec/fixtures/pv.yml ~/.pv`
-    end
+      subject { Pv::Configuration.new }
 
-    subject { Pv::Configuration.new }
+      it "reads from ~/.pv YAML" do
+        subject.should be_present
+        subject.attributes.should_not be_empty
+        subject.attributes.count.should == 4
+      end
 
-    it "reads from ~/.pv YAML" do
-      subject.should be_present
-      subject.attributes.should_not be_empty
-      subject.attributes.count.should == 4
-    end
+      it "parses a username" do
+        subject.username.should == "johndoe"
+      end
 
-    it "parses a username" do
-      subject.username.should == "johndoe"
-    end
+      it "parses a password" do
+        subject.password.should == "password"
+      end
 
-    it "parses a password" do
-      subject.password.should == "password"
-    end
+      it "parses a full name" do
+        subject.name.should == "John Doe"
+      end
 
-    it "parses a full name" do
-      subject.name.should == "John Doe"
-    end
+      it "parses a project id into a number" do
+        subject.project_id.should == 123456
+      end
 
-    it "parses a project id into a number" do
-      subject.project_id.should == 123456
-    end
-
-    after do
-      if File.exists? File.expand_path("~/.pv.safe")
-        `cp ~/.pv.safe ~/.pv`
+      after do
+        if File.exists? File.expand_path("~/.pv.safe")
+          `cp ~/.pv.safe ~/.pv`
+        end
       end
     end
+
+    context "Using the project configuration" do
+      before do
+        `cp #{Dir.pwd}/spec/fixtures/pv.project.yml #{Dir.pwd}/.pv`
+      end
+
+      subject { Pv::Configuration.new }
+
+      it "reads from ~/.pv YAML" do
+        subject.should be_present
+        subject.attributes.should_not be_empty
+        subject.attributes.count.should == 4
+      end
+
+      it "parses a username" do
+        subject.username.should == "johndoe"
+      end
+
+      it "parses a password" do
+        subject.password.should == "password"
+      end
+
+      it "parses a full name" do
+        subject.name.should == "John Doe"
+      end
+
+      it "parses a project id into a number" do
+        subject.project_id.should == 78901
+      end
+
+      after do
+        `rm -rf #{Dir.pwd}/.pv`
+      end
+    end
+
   end
 end
